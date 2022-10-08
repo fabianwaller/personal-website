fbad918861dbde90302a2b4984ed7c780f85806b
Fabian Waller <fabianwaller@icloud.com>
2022-10-08 23:51:23 +0200

    api fix (#14)
    path fix

--- src/server/controllers/app.js
+++ src/server/controllers/app.js
@@ -1,3 +1,8 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const serveApp = () => async (req, res) => {
-    res.sendFile(path.join(__dirname, '../../dist/client/index.html'))
+    res.sendFile(path.join(__dirname, '../../../dist/client/index.html'))
 }
