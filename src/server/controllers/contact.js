e79c665c0941542d420ac6c65b40af73bfc7cd30
fabianwaller <fabianwaller@icloud.com>
2022-10-10 09:29:40 +0200

    contact backend

--- src/server/controllers/contact.js
+++ src/server/controllers/contact.js
@@ -3,36 +3,34 @@ import { sendMail } from '../helpers/email.js'
 
 export const handleContact = () => async (req, res) => {
 
-    let mongoClient;
+    let cluster;
     try {
-        insertContact(mongoClient);
+        cluster = new Cluster();
+        let mongoClient = cluster.getMongoClient();
+        const db = mongoClient.db('personal-website');
+        const contact = db.collection('contact');
+
+        const data = {
+            name: req.body.name,
+            email: req.body.email,
+            message: req.body.message
+        }
+
+        console.log(data);
+
+        await contact.insertOne(data);
     } finally {
-        disconnectCluster(mongoClient);
+        cluster.disconnect();
     }
 
     const mailOptions = {
         from: req.body.email,
         to: process.env.MAILLIST,
-        subject: `${req.body.subject}`,
+        subject: `web contact`,
         text: `${req.body.email}: ${req.body.message}`
     }
 
-    sendMail(mailOptions);
+    //sendMail(mailOptions);
 
     return res.json('Message sent');
-}
-
-const insertContact = async (mongoClient) => {
-    mongoClient = await connectCluster();
-    const db = mongoClient.db('personal-website');
-    const contact = db.collection('contact');
-
-    const data = {
-        name: req.body.name,
-        email: req.body.email,
-        subject: req.body.subject,
-        message: req.body.message
-    }
-
-    await contact.insertOne(data);
 }
