9a082ce189d7b42a22bd659726b603c27c16c330
fabianwaller <mail@fabianwaller.de>
2022-11-20 23:37:56 +0100

    email transporter

--- src/server/helpers/email.js
+++ src/server/helpers/email.js
@@ -1,10 +1,9 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
-    //host: process.env.EMAIL_HOST,
-    //port: process.env.EMAIL_PORT,
-    //secure: false,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
