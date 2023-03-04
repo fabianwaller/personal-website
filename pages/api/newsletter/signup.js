7abcf04adbf60168b376c0faa29b4b5eef941f7b
Fabian Waller <mail@fabianwaller.de>
2023-03-04 14:24:06 +0100

    projects server side props (#36)
    * test
    
    * async send mail
    
    * moved transporter creation
    
    * test
    
    * cleanup
    
    * vercel analytics
    
    * loading projects server side

--- pages/api/newsletter/signup.js
+++ pages/api/newsletter/signup.js
@@ -1,6 +1,3 @@
-import {fileURLToPath} from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 import fs from 'fs'
 import path from 'path';
 import Cluster from '../helpers/cluster.js';
