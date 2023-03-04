b98e48f1e8514af14ec30142ae871c2545c83ab9
Fabian Waller <mail@fabianwaller.de>
2023-03-04 13:18:48 +0100

    vercel analytics (#35)
    * test
    
    * async send mail
    
    * moved transporter creation
    
    * test
    
    * cleanup
    
    * vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/styles.scss'
+import {Analytics} from '@vercel/analytics/react'
 
 const App = ({Component, pageProps}) => {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    )
 }
 
 export default App
