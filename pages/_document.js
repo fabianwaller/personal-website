74263d65371c9fa157ad196e45d082bfe5332bb1
Fabian Waller <mail@fabianwaller.de>
2023-03-05 20:54:06 +0100

    meta data in document

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,23 @@
 import {Html, Head, Main, NextScript} from 'next/document'
 
+const siteTitle = 'fabianwaller';
+
 const Document = () => {
     return (
         <Html>
             <Head>
+                <title>{siteTitle}</title>
+                <meta name="og:title" content={siteTitle} />
+                <meta
+                    property="og:image"
+                    content="https://www.fabianwaller.de/logo.svg"
+                />
+
                 {/* <!-- Favicons --> */}
                 <link rel="icon" href="/logo.svg" type="image/svg+xml" />
                 <link rel="mask-icon" href="mask-icon.svg" color="#000000" />
                 <link rel="apple-touch-icon" href="logo.png" />
-                <meta name="theme-color" content="#ffffff" />
+                {/* <meta name="theme-color" content="#ffffff" /> */}
                 <link rel="manifest" href="manifest.json"></link>
 
                 {/* <!-- Icons --> */}
