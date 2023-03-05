01b1a09bf65f1d27037a1d479abc83881072b016
Fabian Waller <mail@fabianwaller.de>
2023-03-05 22:46:53 +0100

    invalid email test

--- test/newsletter.test.js
+++ test/newsletter.test.js
@@ -7,6 +7,30 @@ describe('handle newsletter signup', () => {
                 host: "localhost:3000"
             },
             body: {
+                email: "djfvkfsdn"
+            }
+        }
+        const res = {
+            status: (code) => {
+                expect(code).toBe(400)
+                return {
+                    json: (data) => {
+                        expect(data).toBe('invalid email address')
+                    }
+                }
+            }
+        }
+        await handleNewsletterSignup(req, res)
+    })
+})
+
+describe('handle newsletter signup', () => {
+    test('should return 200 OK', async () => {
+        const req = {
+            headers: {
+                host: "localhost:3000"
+            },
+            body: {
                 email: "dollar_akkorde_0s@icloud.com"
             }
         }
