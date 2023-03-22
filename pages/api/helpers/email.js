6df5d7bdcd6585ac29f48e8312a6f8a451286340
Fabian Waller <mail@fabianwaller.de>
2023-03-22 11:02:45 +0100

    email transporter test

--- pages/api/helpers/email.js
+++ pages/api/helpers/email.js
@@ -1,16 +1,17 @@
 import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASSWORD
-    }
-})
 
 const sendMail = async (mailOptions) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    })
+
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             console.log(error)
