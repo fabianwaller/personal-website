15f4266493bd9e5b6185dbcf1e012785a81907a9
Fabian Waller <mail@fabianwaller.de>
2025-03-31 20:31:08 +0200

    not found center + button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,19 @@
 import VStack from "@/components/VStack";
 import Container from "@/components/Container";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const PageNotFound = () => {
   return (
     <Container fullScreen center>
-      <VStack>
-        <h1> 404 - Page Not Found</h1>
-        <small>The page you are looking for does not exist.</small>
+      <VStack className="gap-6">
+        <VStack className="text-center">
+          <h1>Page Not Found</h1>
+          <span>The page you are looking for does not exist.</span>
+        </VStack>
+        <Link href={"/"}>
+          <Button>Bring me back</Button>
+        </Link>
       </VStack>
     </Container>
   );
